e1f5432d79d9571613f7a6644e9d405f58c73be3
Alan Villa <a19i23villa@gmail.com>
2017-03-14 20:06:20 -0500

    added slack channel functionality

--- moveYourCarv2.py
+++ moveYourCarv2.py
@@ -3,6 +3,10 @@ import datetime
 import re
 from bs4 import BeautifulSoup
 from bs4 import Tag
+from slacker import Slacker
+
+# credentials file not in git
+import credentials
 
 # today's date
 date = datetime.datetime.today().strftime('%-m/%d/%Y')
@@ -21,10 +25,10 @@ soup = BeautifulSoup(page, 'html.parser')
 all_rows = soup.find_all('tr', class_='schedule_home_tr')
 
 # see if any game is today
-# entryForToday = [t for t in all_rows if t.findAll('nobr',text=re.compile('.*({}).*'.format(date)))]
+entryForToday = [t for t in all_rows if t.findAll('nobr',text=re.compile('.*({}).*'.format(date)))]
 
 # hard coding for testing weekend
-entryForToday = [t for t in all_rows if t.findAll('nobr',text=re.compile('3/14/2017'))]
+# entryForToday = [t for t in all_rows if t.findAll('nobr',text=re.compile('3/14/2017'))]
 
 classForTime = "schedule_dgrd_time/result"
 timeOfGame = "none";
@@ -33,14 +37,17 @@ if entryForToday:
     entryForToday = [t for t in entryForToday if t.findAll('td',
                                                             class_='schedule_dgrd_game_day_of_week',
                                                             text=re.compile('.*({}).*'.format(validDay)))]
-    if entryForToday:
-        for elements in entryForToday:
-            for element in elements:
+    for elements in entryForToday:
+        for element in elements:
 
-                if isinstance(element, Tag):
-                    if element.attrs['class'][0] == classForTime:
-                        timeOfGame = element.text
-                    # print element.text
-                        break
+            if isinstance(element, Tag):
+                if element.attrs['class'][0] == classForTime:
+                    timeOfGame = element.text
+                    break
 
 print timeOfGame
+
+#send to slack channel
+message = 'There\'s a game today that starts at ' + timeOfGame
+slack = Slacker(credentials.slackAPIToken)
+slack.chat.post_message(credentials.user, message)
